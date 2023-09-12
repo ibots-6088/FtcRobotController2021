b45d73035c44539eca38d641b4d3cb085bdaf03b
AstroBoy6088 <115894020+AstroBoy6088@users.noreply.github.com>
2023-09-12 16:31:16 -0700

    Create_Auto24_Programs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto24BlueBackstage.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Auto24BlueBackstage", group="Autonomous")
+public class Auto24BlueBackstage extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lf = null;  //left front wheel
+    private DcMotor rf = null;  //right front wheel
+    private DcMotor lb = null;  //left back wheel
+    private DcMotor rb = null;  //right back wheel
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+        lb = hardwareMap.get(DcMotor.class, "lb");
+        rb = hardwareMap.get(DcMotor.class, "rb");
+
+        lf.setDirection(DcMotor.Direction.FORWARD);
+        rf.setDirection(DcMotor.Direction.REVERSE);
+        lb.setDirection(DcMotor.Direction.FORWARD);
+        rb.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive() && (runtime.seconds() < 30.0)){
+
+        }
+    }
+}
